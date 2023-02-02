c5324b7ad7f09db3742d9177ef63665ca14a62ce
zubeirarhesta <zubeirarhesta@gmail.com>
2023-02-02 22:39:10 +0700

    pushing FractionToken.t.sol

--- test/FractionToken.t.sol
+++ test/FractionToken.t.sol
@@ -9,7 +9,7 @@ contract FractionTokenTest is Test {
 
     function setUp() public virtual {
         fractionToken = new FractionToken(
-            0xa0Ee7A142d267C1f36714E4a8F75612F20a79720,
+            0xa0Ee7A142d267C1f36714E4a8F75612F20a79720, // anvil preset address
             5000,
             address(msg.sender),
             10,
@@ -31,14 +31,14 @@ contract FractionTokenTest is Test {
 
     function test_getBalances() public {
         fractionToken.mintTo(address(this), 1000);
-        address five = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc;
+        address five = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc; // anvil preset address
         fractionToken.transferTo(five, 200, 2);
         assertEq(fractionToken.getBalanceOf(five), 200);
     }
 
     function test_twoGetBalances() public {
         fractionToken.mintTo(address(this), 1000);
-        address seven = 0x14dC79964da2C08b23698B3D3cc7Ca32193d9955;
+        address seven = 0x14dC79964da2C08b23698B3D3cc7Ca32193d9955; // anvil preset address
         fractionToken.transferTo(seven, 300, 2);
         assertFalse(
             fractionToken.getBalanceOf(seven) ==
