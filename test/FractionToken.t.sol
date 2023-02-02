de13e2b2cb973144361dd2a6d32bbd796bfe4b04
zubeirarhesta <zubeirarhesta@gmail.com>
2023-02-02 22:38:10 +0700

    pushing FractionToken.t.sol

--- test/FractionToken.t.sol
+++ test/FractionToken.t.sol
@@ -6,9 +6,6 @@ import "src/1.0/FractionToken.sol";
 
 contract FractionTokenTest is Test {
     FractionToken internal fractionToken;
-    address buyer = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-    uint256 tokens = 100;
-    uint256 tokenNum = 4444;
 
     function setUp() public virtual {
         fractionToken = new FractionToken(
